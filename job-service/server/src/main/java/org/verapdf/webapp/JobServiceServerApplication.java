686a482731187fc5e5f3f4caff0618d1512a4ff7
Roman <romasemizon@gmail.com>
2020-04-09 13:02:18 +0300

    Added one more package webapp.

---
+++ job-service/server/src/main/java/org/verapdf/webapp/JobServiceServerApplication.java
@@ -0,0 +1,11 @@
+package org.verapdf.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobServiceServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobServiceServerApplication.class, args);
+    }
+}
