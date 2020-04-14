1b95ab3b316d2431af8b92ecf2cfa190a095ac29
Roman Semizhon <42668572+RomaPrograms@users.noreply.github.com>
2020-04-14 11:04:13 +0300

    Added job-service model for communication with workers. (#27)

---
+++ job-service/model/src/main/java/org/verapdf/webapp/model/dto/ExecutableTaskDTO.java
@@ -0,0 +1,73 @@
+package org.verapdf.webapp.model.dto;
+
+import org.verapdf.webapp.model.entity.enums.Profile;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class ExecutableTaskDTO {
+	private UUID jobId;
+	private UUID fileId;
+	private Profile profile;
+
+	public ExecutableTaskDTO() {
+	}
+
+	public ExecutableTaskDTO(UUID jobId, UUID fileId, Profile profile) {
+		this.jobId = jobId;
+		this.fileId = fileId;
+		this.profile = profile;
+	}
+
+	public UUID getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(UUID jobId) {
+		this.jobId = jobId;
+	}
+
+	public UUID getFileId() {
+		return fileId;
+	}
+
+	public void setFileId(UUID fileId) {
+		this.fileId = fileId;
+	}
+
+	public Profile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof ExecutableTaskDTO)) {
+			return false;
+		}
+		ExecutableTaskDTO that = (ExecutableTaskDTO) o;
+		return Objects.equals(jobId, that.jobId) &&
+				Objects.equals(fileId, that.fileId) &&
+				profile == that.profile;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(jobId, fileId, profile);
+	}
+
+	@Override
+	public String toString() {
+		return "ExecutableTaskDTO{" +
+				"jobId=" + jobId +
+				", fileId=" + fileId +
+				", profile=" + profile +
+				'}';
+	}
+}
