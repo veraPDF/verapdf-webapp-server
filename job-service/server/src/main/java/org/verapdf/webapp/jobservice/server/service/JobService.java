d8369a4abdce8369048c0b50c27208a449336cb1
irinamavrina <irina.mavrina@duallab.com>
2022-09-28 12:27:08 +0300

    Added logs for job processing tracking

--- job-service/server/src/main/java/org/verapdf/webapp/jobservice/server/service/JobService.java
+++ job-service/server/src/main/java/org/verapdf/webapp/jobservice/server/service/JobService.java
@@ -145,6 +145,7 @@ public class JobService {
 
 			ExecutableTaskDTO executableTaskDTO = new ExecutableTaskDTO(jobId, task.getFileId());
 			try {
+				LOGGER.info("Enter queue: {} ", executableTaskDTO.getJobId());
 				queueSender.sendMessage(objectMapper.writeValueAsString(executableTaskDTO));
 				task.setStatus(TaskStatus.PROCESSING);
 			} catch (JsonProcessingException e) {
